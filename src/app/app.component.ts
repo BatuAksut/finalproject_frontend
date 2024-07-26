c6836a538d4dcbec634ad5b05927cdacc1b92779
Batuhan Aksüt <batuhanaksut@hotmail.com>
2024-07-26 20:21:04 +0300

    components created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet} from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { CategoryComponent } from "./components/category/category.component";
+import { NaviComponent } from "./components/navi/navi.component";
+import { ProductComponent } from "./components/product/product.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, CategoryComponent, NaviComponent, ProductComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'northwind';
+  title:string= "Northwind";
+  user:string="Batuhan Aksüt";
+  
 }
+
+
